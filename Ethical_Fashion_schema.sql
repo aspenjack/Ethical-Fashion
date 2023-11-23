70fc4a412462d9f152d69242d114b6d44ce1dd35
velvetklr-iHGRgW <velvetklr@gmail.com>
2023-11-22 22:21:43 -0800

    adding database schema

---
+++ Ethical_Fashion_schema.sql
@@ -0,0 +1,52 @@
+--country_summary
+create table country_summary (
+    Country varchar(100) primary key,
+    Overall_Mean numeric not null,
+    Sustainable_Sourcing_Mean numeric not null,
+    Overconsumption_Mean numeric not null,
+    Deforestation_Mean numeric not null,
+    Fair_Wages_Mean numeric not null
+);
+
+--company_cleaned_df
+create table company_country (
+    Company varchar(80) primary key,
+    ID int not null,
+    State varchar(50),
+    Country varchar(100) not null,
+    foreign key (Country) references country_summary (Country)
+);
+
+--cleaned_df
+create table company_metric (
+    Company varchar(80) not null,
+    Year int not null,
+    Value numeric not null,
+    Metric varchar(400) not null,
+    primary key (Company, Metric),
+    foreign key (Company) references company_country(Company)
+);
+
+--combined_df
+create table company_scores (
+    Company varchar(80),
+    Year int not null,
+    Value numeric not null,
+    Metric varchar (400),
+    ID int not null,
+    State varchar(50),
+    Country varchar(100),
+    primary key (Metric, Company),
+    foreign key (Company) references company_country(Company)
+);
+
+--company_summary
+create table company_summary (
+    Company varchar(80) primary key,
+    Overall_Mean numeric not null,
+    Sustainable_Sourcing_Mean numeric not null,
+    Overconsumption_Mean numeric not null,
+    Deforestation_Mean numeric not null,
+    Fair_Wages_Mean numeric not null,
+    foreign key (Company) references company_country(Company)
+);
