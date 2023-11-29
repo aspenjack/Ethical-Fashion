2332dbb8b5494ab4f8faaf01766545b7aa51616a
Christina Ashley Zermeno <christinaashleyzermeno@Christinas-MacBook-Air.local>
2023-11-28 20:59:23 -0800

    worked on html and logic to fix errors

--- logic.js
+++ logic.js
@@ -1,95 +1,12 @@
-// const apiUrl = 'http://127.0.0.1:5001/api/v1.0/company_summary';
-// const countrySummary = 'http://127.0.0.1:5001/api/v1.0/country_summary';
-
-
-// function buildCharts(){
-// d3.json(countrySummary, { mode: 'cors' })
-//     .then((data) => { console.log(data)
-//         let x  = data.map((d)=>d.country)
-//         console.log(x)
-//         let y1  = data.map((d)=>d.overall_mean)
-//         console.log(y1)
-//         let y2  = data.map((d)=>d.deforestation_mean)
-//         console.log(y2)
-//         let y3  = data.map((d)=>d.sustainable_sourcing_mean)
-//         console.log(y3) 
-//         let y4  = data.map((d)=>d.overconsumption_mean)
-//         console.log(y4)
-//         let y5 = data.map((d)=>d.fair_wages_mean)
-//         console.log(y5)
-
-//         let trace1 = [
-//             {
-//               x: x,
-//               y: y1,
-//               type: 'bar'
-//             }
-//           ];
-
-//         let trace1Layout = {
-//             title: "Overall Sustainability Scores by Country",
-//             margin: { t:100},
-//             hovermode: "closest",
-//             xaxis: { title: "Countries"},
-//             margin: { t: 100}
-//         };
-    
-//         Plotly.newPlot('plot', trace1, trace1Layout)
-
-    
-
-    
-//         // for (let d in data) {
-//         //     let test = data[d]
-//         //     // console.log(test)
-//         //     let overall = [test.overall_mean];
-//         //     console.log(topValues);
-//         // }
-    
-    
-//     })
-
-
-
-
-// }
-// function init(){
-//     let selector = d3.select("#selDataset");
-
-//     d3.json(countrySummary, { mode: 'cors' }).then((data) => {
-//         // let x  = data.map((d)=>d.country)
-//         for (let d in data){
-//             selector.append("option").text(data[d]).property("value", data[d]);
-//         }
-
-        
-
-//         let firstSample = data[0];
-//         buildCharts(firstSample);
-//     })
-
-    
-// }
-// function optionChanged(newSample){
-//     buildCharts(newSample);
-// }
-// buildCharts()
-// init()
-
 const apiUrl = 'http://127.0.0.1:5001/api/v1.0/company_summary';
 const countrySummary = 'http://127.0.0.1:5001/api/v1.0/country_summary';
+const companySummary = "http://127.0.0.1:5001/api/v1.0/company_summary"
 function buildCharts(selectedMetric) {
     d3.json(countrySummary, { mode: 'cors' })
         .then((data) => {
             //console.log(data);
             // Assuming data is an array of objects with properties like 'country', 'overall_mean', etc.
-            let test = data.map((d) => d)
-            for (let t in test) {
-                let test3 = test[t]
-                const { country, deforestation_mean, fair_wages_mean, overall_mean, overconsumption_mean, sustainable_sourcing_mean } = test3
-                let deforest = deforestation_mean >3
-                console.log(deforest,deforestation_mean)
-            }
+            
 
             let x = data.map((d) => d.country);
             let y;
@@ -133,7 +50,7 @@ function buildCharts(selectedMetric) {
                 xaxis: { title: 'Countries',
                 tickangle: -45},
                 yaxis: { range: [0,10],
-                    title: 'Score (0-10)'},
+                    title: 'Metric Score (0-10)'},
                 automargin: true,
             };
             Plotly.newPlot('plot', trace1, trace1Layout);
@@ -148,12 +65,99 @@ function init() {
     selector.on('change', function () {
         let selectedMetric = d3.select(this).property('value');
         buildCharts(selectedMetric);
+        buildCharts2(selectedMetric);
+        buildCharts2(selectedMetric2);
     });
     // Fetch initial data with default metric
     buildCharts('overall_mean');
 }
-init();
 
+function buildCharts2(selectedMetric, selectedMetric2) {
+    d3.json(companySummary,{ mode: 'cors'})
+        .then((response) => {
+            let company_sum = response.map((d) => d)
+            let company_overall = company_sum.filter((r)=> r.overall_mean > 5)
+            let company_sustainable = company_sum.filter((r)=> r.sustainable_sourcing_mean > 5)
+            let company_overconsumption = company_sum.filter((r)=> r.overconsumption_mean > 5)
+            let company_deforestation = company_sum.filter((r)=> r.deforestation_mean > 5)
+            let company_fair_wages = company_sum.filter((r)=> r.fair_wages_mean > 5)
+
+
+                
+            let test = company_overall.map((d)=>d.overall_mean)
+            console.log(test)
+
+            let x;
+            switch (selectedMetric) {
+                case 'overall_mean':
+                    x = company_overall.map((d) => d.overall_mean);
+                    break;
+                case 'sustainable_sourcing_mean':
+                    x = company_sustainable.map((d) => d.sustainable_sourcing_mean);
+                    break;
+                case 'overconsumption_mean':
+                    x = company_overconsumption.map((d) => d.overconsumption_mean);
+                    break;
+                case 'deforestation_mean':
+                    x = company_deforestation.map((d) => d.deforestation_mean);
+                    break;
+                case 'fair_wages_mean':
+                    x = company_fair_wages.map((d) => d.fair_wages_mean);
+                    break;
+                default:
+                    x = company_overall.map((d) => d.overall_mean); // Default to overall mean
+            }
+            let y;
+            // Determine which metric to use based on the selected value
+            switch (selectedMetric2) {
+                case 'overall_mean':
+                    y = company_overall.map((d) => d.company);
+                    break;
+                case 'sustainable_sourcing_mean':
+                    y = company_sustainable.map((d) => d.company);
+                    break;
+                case 'overconsumption_mean':
+                    y = company_overconsumption.map((d) => d.company);
+                    break;
+                case 'deforestation_mean':
+                    y = company_deforestation.map((d) => d.company);
+                    break;
+                case 'fair_wages_mean':
+                    y = company_fair_wages.map((d) => d.company);
+                    break;
+                default:
+                    y = company_overall.map((d) => d.company); // Default to overall mean
+            }
+            let trace2 = [
+                {
+                    x: x,
+                    y: y,
+                    marker: {color: '#4e8983'},
+                    type: 'bar',
+                    orientation: 'h',
+                    text: x.map(value => value.toFixed(1)),
+                    textposition: 'outside',
+                },
+            ];
+            let trace2Layout = {
+                title: { text: "Companies That Scored<br> Between 5-10 on Sustainability Metrics<br> " },
+                margin: { t: 50,
+                b:50,
+                l:200,
+                r:0 },
+                hovermode: 'closest',
+                xaxis: { range: [0,11],
+                    title: 'Metric Score (0-10)'},
+                automargin: true,
+                barmode: 'group',
+            };
+            Plotly.newPlot('plot1', trace2, trace2Layout);
+    
+
+        })
+}
+init();
+buildCharts2()
 //React
 
 //Reply
